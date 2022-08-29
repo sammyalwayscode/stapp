10f6af9f72bfc85d4c66f5e234ce64517cefd49f
sammyalwayscode <olorundasamuel2@gmail.com>
2022-08-29 17:54:27 +0100

    live server

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// const MONGOATLAS_URI =
-//   "mongodb+srv://W8PypVqIRJXDReMh:W8PypVqIRJXDReMh@cluster0.1nq2x.mongodb.net/sktriumph?retryWrites=true&w=majority";
 const MONGOCOMPASS_URI = "mongodb://localhost/skintriumph";
 const MAIN_ATLAS = process.env.MAIN_ATLAS_URI;
 
-mongoose.connect(MONGOCOMPASS_URI);
+mongoose.connect(MAIN_ATLAS);
 mongoose.connection
   .on("open", () => {
     console.log("Connected to DB🏬");
